76f6c0dd38020ad6a7c84368314f47af033f5e54
JoAn <joannacalinska@gmail.com>
2019-12-16 21:29:35 +0100

    move home comp

---
+++ src/Home.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Joke from './Joke';
+import Stories from './Stories';
+import Tasks from './Tasks';
+import Gallery from './Gallery';
+import Matrix from './Matrix';
+import Menu from './Menu';
+
+function Home() {
+  const [userQuery, setUserQuery] = useState('');
+  const [showGallery, setShowGallery] = useState(true);
+
+  const toggleGallery = () => {
+    setShowGallery(!showGallery);
+  };
+
+  const updateUserQuery = event => {
+    setUserQuery(event.target.value);
+  };
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  };
+  return (
+    <div>
+      <Menu />
+      <h1>Hello Joanna</h1>
+      <h2>What do you want to discover today?</h2>
+      <div className="form">
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+      <hr />
+      <Joke />
+      <hr />
+      <Tasks />
+      <hr />
+      <Matrix />
+      <hr />
+      <div>
+        {showGallery && <Gallery />}
+        <button onClick={toggleGallery}>
+          {showGallery ? 'Hide' : 'Show'} gallery
+        </button>
+      </div>
+      <hr />
+      <Stories />
+    </div>
+  );
+}
+
+export default Home;
