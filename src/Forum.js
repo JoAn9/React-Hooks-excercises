eb45aea2fa2d5d9e32d67fe1512dbe62acdfa627
JoAn <joannacalinska@gmail.com>
2019-12-16 21:29:54 +0100

    add menu & forum comp

---
+++ src/Forum.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Forum() {
+  return (
+    <div>
+      <h2>Forum</h2>
+    </div>
+  );
+}
+
+export default Forum;
---
+++ src/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Menu() {
+  return (
+    <div className="Menu">
+      <Link to="/forum">Forum</Link>
+    </div>
+  );
+}
+
+export default Menu;
