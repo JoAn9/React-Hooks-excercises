667dd0e60de3951996ab1fd5665a13f5ba1c2c4c
JoAn <joannacalinska@gmail.com>
2019-12-20 21:38:41 +0100

    add username to msg

--- src/components/wall/MessageBoard.js
+++ src/components/wall/MessageBoard.js
@@ -8,11 +8,12 @@ function MessageBoard() {
   return (
     <ul>
       {messages.map(item => {
-        const { text, id, timestamp } = item;
+        const { id, text, username, timestamp } = item;
         return (
           <li key={id}>
             <h4>{new Date(timestamp).toLocaleString()}</h4>
             <p>{text}</p>
+            <p>- {username ? username : 'anonymus'}</p>
             <hr />
           </li>
         );
